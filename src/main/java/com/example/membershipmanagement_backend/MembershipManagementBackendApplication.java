713a7e614edbf6e5d0e6aaf01cc3a1aecc492b23
kimkyoungwook <uk7764@naver.com>
2024-03-09 14:10:59 +0900

    🎉membership-management_backend Start

---
+++ src/main/java/com/example/membershipmanagement_backend/MembershipManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.membershipmanagement_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MembershipManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/membershipmanagement_backend/MembershipManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.membershipmanagement_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
